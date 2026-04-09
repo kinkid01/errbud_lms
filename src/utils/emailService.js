5b12ebed80cdcf13a33c120369c011dd5837de80
Michael <michaelayegbamartins4real@gmail.com>
2026-04-09 22:08:57 +0100

    feature:gmail-fixing

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -10,9 +10,6 @@ const generateVerificationToken = () => {
 const createTransporter = () => {
   return nodemailer.createTransport({
     service: 'gmail',
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_SECURE === 'true',
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
