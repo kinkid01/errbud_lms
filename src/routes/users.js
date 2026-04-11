5324d31d591ab242003b32962e019c3fc8040caa
Michael <michaelayegbamartins4real@gmail.com>
2026-04-11 15:02:56 +0100

    feature:student-management-fix-2

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,11 +5,11 @@ const { requireRole } = require('../middleware/role');
 const { createStudent, getAllUsers, refreshUserData, updateUserStatus, getUserById, updateUser, deleteUser } = require('../controllers/userController');
 
 router.post('/create-student', protect, requireRole('admin'), createStudent);
-router.get('/refresh/:userId', protect, requireRole('admin'), refreshUserData);
-router.get('/:id',             protect, requireRole('admin'), getUserById);
-router.put('/users/:userId/status', protect, requireRole('admin'), updateUserStatus);
-router.put('/:id',             protect, updateUser);
-router.delete('/users/:userId', protect, requireRole('admin'), deleteUser);
 router.get('/',                protect, requireRole('admin'), getAllUsers);
+router.get('/refresh/:userId', protect, requireRole('admin'), refreshUserData);
+router.put('/users/:userId/status', protect, requireRole('admin'), updateUserStatus);
+router.delete('/users/:userId', protect, requireRole('admin'), deleteUser);
+router.get('/:id',             protect, requireRole('admin'), getUserById);
+router.put('/:id',             protect, updateUser);
 
 module.exports = router;
